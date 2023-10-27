f49e7a4598e8e7ef2ad5474c6918a658fe650dab
nicoIwas <nickuhr@usp.br>
2023-10-26 22:57:30 -0300

    filepath na main (y)

--- cybergari/src/main/Main.java
+++ cybergari/src/main/Main.java
@@ -7,7 +7,8 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String... args) throws IOException {
-        final var test = new LocalFileManager(new SimpleCompressor(), "C:\\Users\\Pedro\\Desktop\\EQ_ST");
+        final String filepath = "C:\\Users\\Pedro\\Desktop\\EQ_ST";
+        final var test = new LocalFileManager(new SimpleCompressor(), filepath);
         final var result = test.getFileStructure();
         System.out.println(test);
     }
