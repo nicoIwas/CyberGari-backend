506f749f0905484fdc40f793c95c33330013bc13
PedroMReno <pedrohmreno@gmail.com>
2023-11-20 20:03:44 -0300

    Update mapper

--- cybergari/src/main/report/FIleReportMapper.java
+++ cybergari/src/main/report/FIleReportMapper.java
@@ -2,16 +2,12 @@ package main.report;
 
 import main.file.File;
 import main.report.vo.FileReport;
+import main.user.UserData;
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
 import org.springframework.stereotype.Component;
 
-@Component
-public class FIleReportMapper {
-    public FileReport mapToReport(final File file) {
-        return new FileReport(
-                file.getId(),
-                file.getName(),
-                file.getSize(),
-                file.getModifiedTime()
-        );
-    }
+@Mapper(componentModel = "spring")
+public interface FIleReportMapper {
+    FileReport mapToReport(File file);
 }
