1942b3b8efedf69d96cd1007108b48be8e89ea2d
PedroMReno <pedrohmreno@gmail.com>
2023-11-20 21:29:12 -0300

    Fix build error

--- cybergari/src/main/report/vo/ReportConfirmation.java
+++ cybergari/src/main/report/vo/ReportConfirmation.java
@@ -1,12 +1,7 @@
 package main.report.vo;
 
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-
 import java.util.List;
 
-@Getter
-@RequiredArgsConstructor
 public record ReportConfirmation(List<String> filesToCompress, List<String> filesToDelete, String reportId,
                                  String userId) {
 }
