7b6c20402450e2a43d3f67a1d147d6ec6e60fa6f
Kailani Esteves <kailaniesteves@gmail.com>
2023-12-17 15:48:02 -0300

    Fixing pipeline

--- src/main/java/com/cybergari/mvp/filemanager/local/LocalFileSettings.java
+++ src/main/java/com/cybergari/mvp/filemanager/local/LocalFileSettings.java
@@ -3,7 +3,7 @@ package com.cybergari.mvp.filemanager.local;
 public class LocalFileSettings {
     private static final SO soInUse = SO.LINUX;
     public static final String SEPARATOR =  "/";
-    public static final String FOLDER = "tests/";
+    public static final String FOLDER = ".github/testFiles";
 
     private enum SO {
         WINDOWS,
