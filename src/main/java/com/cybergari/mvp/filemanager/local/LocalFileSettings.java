37f8cef8f89721a580d6cc87ca0832f6229e0fde
Kailani Esteves <kailaniesteves@gmail.com>
2023-12-17 15:37:48 -0300

    Restructuring repo

--- src/main/java/com/cybergari/mvp/filemanager/local/LocalFileSettings.java
+++ src/main/java/com/cybergari/mvp/filemanager/local/LocalFileSettings.java
@@ -2,8 +2,8 @@ package com.cybergari.mvp.filemanager.local;
 
 public class LocalFileSettings {
     private static final SO soInUse = SO.LINUX;
-    public static final String SEPARATOR =  "\\";
-    public static final String FOLDER = "C:\\Users\\Note\\Desktop\\USP\\BD2";
+    public static final String SEPARATOR =  "/";
+    public static final String FOLDER = "tests/";
 
     private enum SO {
         WINDOWS,
