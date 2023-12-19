c6778c3e4793e4094288d2cc75c4452402ff2598
Kailani Esteves <kailaniesteves@gmail.com>
2023-12-18 23:17:15 -0300

    Updating localFileSettings

--- src/main/java/com/cybergari/mvp/filemanager/local/LocalFileSettings.java
+++ src/main/java/com/cybergari/mvp/filemanager/local/LocalFileSettings.java
@@ -2,11 +2,12 @@ package com.cybergari.mvp.filemanager.local;
 
 public class LocalFileSettings {
     private static final SO soInUse = SO.LINUX;
-    public static final String SEPARATOR =  "\\";
-    public static final String FOLDER = "C:\\Users\\Note\\Desktop\\USP\\BD2";
+    public static final String SEPARATOR = "/";
+    public static final String FOLDER = ".github/testFiles";
 
     private enum SO {
         WINDOWS,
         LINUX
     }
 }
+
